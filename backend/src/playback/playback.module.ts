3064de57833b5f24f01bbabc2020af89c8298180
Bolotinhas <glodsonborges@gmail.com>
2026-05-17 14:13:51 -0300

    test: implement playback module for listening history feature

--- backend/src/playback/playback.module.ts
+++ backend/src/playback/playback.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PlaybackService } from './playback.service';
 import { PlaybackController } from './playback.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Playback } from './entities/playback.entity';
+import { User } from '../users/entities/user.entity';
+import { Musica } from '../musicas/musica.entity';
+import { Episode } from '../episodes/entities/episode.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([
+    Playback,
+    User,
+    Musica,
+    Episode,
+  ]),],
   controllers: [PlaybackController],
   providers: [PlaybackService],
 })
